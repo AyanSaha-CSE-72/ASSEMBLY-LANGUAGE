5735994003c1ff22383fa155f8fc90804f1d207e
Ayan Saha <143374035+AyanSaha-CSE-72@users.noreply.github.com>
2025-06-29 23:06:48 +0600

    Add files via upload

---
+++ add sub lab22.asm
@@ -0,0 +1,62 @@
+include "emu8086.inc"
+
+.stack 100h
+.model small
+.data
+.code
+
+main proc 
+    print 'Enter 1st Number:' 
+    mov ah, 1h
+   int 21h  
+   sub al, 48
+   
+   mov bl,al
+   
+   mov dl,10
+   mov ah,2h
+   int 21h 
+   
+   mov dl,13
+   mov ah,2h
+   int 21h    
+   
+   print 'Enter 2nd Number:' 
+    mov ah, 1h
+   int 21h  
+   sub al, 48 
+   
+   ;add bl,al; bl=bl+al
+   
+   ;sub bl,al ;bl=bl-al 
+   
+   ;mul bl ;bl=bl*al   
+   
+   div bl
+   mov bl,al
+   
+   add bl,48
+           
+      
+   mov dl,10
+   mov ah,2h
+   int 21h
+   
+      
+   mov dl,10
+   mov ah,2h
+   int 21h
+   
+   print 'The sum is:'
+   mov dl,bl 
+   
+   mov ah,2h
+   int 21h       
+           
+   ;Exit program  
+   mov ah, 4ch
+   int 21h
+                                                        
+main  endp
+end main          
+
